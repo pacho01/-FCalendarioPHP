5664bee4e51a5af1feec361581b1bc1dc2d68c79
Lloris Martinez <JLLORIS@ford.com>
2019-03-06 01:20:37 +0100

    Primer paso al calendario total

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-    <html lang="en">
+    <html lang="es">
     <head>
         <meta charset="UTF-8">
         <title>Calendario</title>
         <link href='http://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
-        <style type="text/css">body{margin:0;font-family:Lato;}ul,li{list-style-type:none;margin:0;padding:0;}.calendar{padding:30px;}.calendar .day{background:#ecf0f1;border-bottom:2px solid #bdc3c7;float:left;margin:3px;position:relative;height:120px;width:120px;}.day.marked{background:#3498db;border-color:#2980b9;}.day .day-number{color:#7f8c8d;left:5px;position:absolute;top:5px;}.day.marked .day-number{color:white;}.day .events{color:white;margin:29px 7px 7px;height:78px;overflow-x:hidden;overflow-y:hidden;}.day .events h5{margin:0 0 5px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap;width:100%;}.day .events strong,.day .events span{display:block;font-size:11px;}.day .events ul{}.day .events li{}</style>
+        <link rel='stylesheet' href='style.css'>
     </head>
     <body>
     <div class="calendar">
     <?php
-
+        // Configuracion de conexion a la base de datos
         $mysqli = new mysqli('localhost', 'root', '', 'calendario');
 
         if ( $mysqli->connect_errno )
@@ -21,7 +21,8 @@
 
         if( !$result )
             die( $mysqli->error );
-
+        
+        // Incializamos un array $events para almacenar los eventos
         $events = array();
 
         while($row = $result->fetch_assoc())
@@ -48,52 +49,65 @@
             'Octubre', 'Noviembre', 'Diciembre'
         );
 
-        $month_txt = $txt_months[$datetime->format('n')-1];
+        /*$month_txt = $txt_months[$datetime->format('n')-1];*/
 
         // ultimo dia del mes
         $month_days = date('j', strtotime("last day of"));
 
-        echo '<h1>' . $month_txt . '</h1>';
+        echo '<h1> Año de nuestro Señor ' . date('Y') . '</h1>';
+        for($mes=1; $mes<=12; $mes++){
 
-        foreach(range(1, $month_days) as $day)
-        {
-            $marked = false;
-            $events_list = array();
+            //Datos de configuracion el mes
+            $month_txt = $txt_months[$mes-1]; //Nombre del MES
+            $month_days = date('t', strtotime("01-$mes-" . date('Y'))); // Numero de dias en el mes
+            $month_ini=date('N', strtotime("01-$mes-" . date('Y'))); // Offset inicio de dia 1 en la semana
 
-            foreach($events as $event_day => $event)
-            {
-                // si el dia del evento coincide lo marcamos y guardamos la informacion
-                if($event_day == $day)
-                {
-                    $marked = true;
-                    $events_list = $event;
-                    break;
-                }
+            echo '<div class="mes">';
+            echo '<h2>' . $month_txt .'</h2>';
+            for($day_free=1; $day_free<$month_ini; $day_free++){
+                echo '<div class="dia_blanco"></div>';
             }
+            foreach(range(1, $month_days) as $day){
+                $marked = false;
+                $events_list = array();
 
-            echo '
-            <div class="day' . ($marked ? ' marked' : '') . '">
-                <strong class="day-number">' . $day . '</strong>
-                <div class="events"><ul>';
-
-                    foreach($events_list as $event)
+                foreach($events as $event_day => $event)
+                {
+                    // si el dia del evento coincide lo marcamos y guardamos la informacion
+                    if($event_day == $day)
                     {
-                        echo '<li>
-                            <h5>' . $event['description'] . '</h5>
-                            <div>
-                                <strong>Inicio:</strong>
-                                <span>' . $event['start_hour'] . '</span>
-                            </div>
-
-                            <div>
-                                <strong>Fin:</strong>
-                                <span>' . $event['end_hour'] . '</span>
-                            </div>
-                        </li>';
+                        $marked = true;
+                        $events_list = $event;
+                        break;
                     }
+                }
 
-                echo '</ul></div>
-            </div>';
+                echo '
+                
+                <div class="day' . ($marked ? ' marked' : '') . '">
+                    <strong class="day-number">' . $day . '</strong>
+                    <div class="events"><ul>';
+
+                        foreach($events_list as $event)
+                        {
+                            echo '<li>
+                                <h5>' . $event['description'] . '</h5>
+                                <div>
+                                    <strong>Inicio:</strong>
+                                    <span>' . $event['start_hour'] . '</span>
+                                </div>
+
+                                <div>
+                                    <strong>Fin:</strong>
+                                    <span>' . $event['end_hour'] . '</span>
+                                </div>
+                            </li>';
+                        }
+
+                    echo '</ul></div>
+                </div>';
+            }
+            echo '</div>';
         }
         ?>
     </div>
