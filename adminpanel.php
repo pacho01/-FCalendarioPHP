6b6f50b66cd1efa82963b186347d86df58f4016e
Lloris Martinez <JLLORIS@ford.com>
2019-03-27 02:52:04 +0100

    En panel administrador marca el usuario seleccionado al entrar en el listado de usuarios

--- adminpanel.php
+++ adminpanel.php
@@ -7,28 +7,37 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<form action="calendario.php" method="Post">
+<form  method="Post">
     <div class="calendar">
         <div class="ficha">
             <h2>Selecciona Usuario</h2>
 
             <?php
+            function rellamada(){
+                
+            }
             session_start();
             require("functions/conexion.php");
             require("functions/funciones.php");
+
             $userid = $_SESSION['user_sesion'];
             $admin=$_SESSION['admin'];
             
-
-            // Selector de usuario
+            if ($_POST){
+                $_SESSION['user_select']=$_POST["userview"];
+                //$result = $mysqli->query("SELECT  * FROM usuarios WHERE userid = '$userview'" );
+            }
+            $userview= $_SESSION['user_select'];   
             $result = $mysqli->query("SELECT  * FROM usuarios" );
+            
+
+            
 
             if(mysqli_num_rows($result)>0):
 
                 echo "
-                <P>
-                <label for='menu'>Usuario</label> 
-                <select formaction='adminpanel.php' formmetod='post' onchange = this.form.submit() class='ficha_bordes' name='menu'>
+                <P><label for='userview'>Sel USER $userid :</label> 
+                <select formaction='adminpanel.php' formmetod='post' onchange = this.form.submit() class='ficha_bordes' name='userview'>
                 ";
 
                 while($row = $result->fetch_assoc())
@@ -36,30 +45,29 @@
                     $menu_nombre = $row['nombre'];
                     //$menu_admin=$row['admin'];
                     $menu_userid=$row['user_id'];
-                    echo "<option value='" . $menu_userid . "'>" . $menu_nombre . "</option>";
+                    $_elemento_seleccionado="";
+                    if($userid == $menu_userid){
+                        $_elemento_seleccionado='selected';
+                        $nombre_usuario=$menu_nombre;
+                        $numero_usuario=$row['numero'];
+
+                    }
+                    echo "<option " . $_elemento_seleccionado . " value='" . $menu_userid . "'>" . $menu_userid . " </option>";
                 }
-                echo "</select></P>";
+                echo "
+                </select></P>";
 
             endif;
 
-            if ($_POST){
-                $Userview=$_POST('userview');
-                $_SESSION['user_select']=$Userview;
-                $result = $mysqli->query("SELECT  * FROM usuarios WHERE userid = '$userview'" );
-                
-            }
-            else {
             
-            $Userview=$_SESSION['user_select'];
-            }
                 # code...
             
         
 
             ?>
-            <p><label for='nombrecito'>Dato extra</label><input class='ficha_bordes' type="text" name="nombrecito" placeholder="Nombre"></p>
-            <p><label for='numero'>Dato extra</label><input class='ficha_bordes' type="text" name="numero" placeholder="Numero E."></p>
-            <p><input type="submit" value="Ver Calendario"> </p>
+            <p><label for='nombrecito'>Nombre USUARIO: </label><input class='ficha_bordes' type="text" name="nombrecito" placeholder= <?php echo $nombre_usuario ?> ></p>
+            <p><label for='numero'>Numero empleado: </label><input class='ficha_bordes' type="text" name="numero" placeholder= <?php echo $nombre_usuario ?> ></p>
+            <p><input type="submit" formaction="calendario.php" value="Ver Calendario"> </p>
             <p><input type="submit" formaction="adminpanel.php" formmetod="post" value="Actualizar"> </p>
             
         </div>
